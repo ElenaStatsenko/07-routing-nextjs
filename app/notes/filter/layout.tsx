6c6d186e6f4a11993d7851aaa21b9c29c0a7857f
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-09-10 21:19:24 +0300

    изменила сайдбар

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,19 +1,16 @@
-import SidebarNotes from "./SidebarNotes";
-
 import css from "./layout.module.css";
 
 export default function LayoutNotes({
   children,
+  sidebar,
 }: Readonly<{
   children: React.ReactNode;
+  sidebar: React.ReactNode;
 }>) {
   return (
     <section className={css.container}>
-      <aside className={css.sidebar}>
-        <SidebarNotes />
-      </aside>
-<div className={css.notesWrapper}>{children}</div>
-     
+      <aside className={css.sidebar}>{sidebar}</aside>
+      <div className={css.notesWrapper}>{children}</div>
     </section>
   );
 }
